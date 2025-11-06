07be2dfeedde89a50c182e4bb4f1a11a0b0840c0
Matheo Duhamel <Matheo.duhamel@etu.unicaen.fr>
2025-11-06 10:43:54 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>MiniChat PHP</title>
+        <link rel="stylesheet" href="styles/style.csss">
+    </head>
+    <body>
+        <h1>MiniChat PHP</h1>
+    </body>
+</html>
