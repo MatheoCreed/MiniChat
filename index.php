b348fb2f52cfd5868867cf1b5e9e5b0d9f970011
Matheo Duhamel <Matheo.duhamel@etu.unicaen.fr>
2025-11-06 11:32:18 +0100

    [ADD + FIX] Ajout css + correction index

--- index.php
+++ index.php
@@ -29,7 +29,7 @@ $messages = $stmt->fetchAll(PDO::FETCH_ASSOC);
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>MiniChat PHP</title>
-        <link rel="stylesheet" href="styles/style.csss">
+        <link rel="stylesheet" href="styles/style.css">
     </head>
     <body>
         <h1>MiniChat PHP</h1>
