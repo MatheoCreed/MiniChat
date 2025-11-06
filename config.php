0d8c64f1c5df51caaebe77bfb7b216276f300331
Matheo Duhamel <Matheo.duhamel@etu.unicaen.fr>
2025-11-06 10:56:35 +0100

    [ADD] Connexion DB

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+
+$db_host = 'localhost';
+$db_name = 'duhamel_minichatphp';
+$db_user = 'root';
+$db_pass = '';
+
+try {
+    $pdo = new PDO("mysql:host=$db_host;dbname=$db_name;charsetutf8", $db_user, $db_pass, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+}   catch (Exception $e){
+    die('Erreur de connexion : ' . htmlspecialchars($e->getMessage()));
+}
+
+function h($s) { return htmlspecialchars($s, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8'); }
+
